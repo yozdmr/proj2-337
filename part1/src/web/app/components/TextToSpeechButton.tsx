2187ac9b89e12dcdcb487158b6a339f2142ea29b
Yusuf Ozdemir <yusuf.ozdemir.0713@gmail.com>
2025-11-19 16:07:30 -0600

    Changed tts play icon when playing from start

--- part1/src/web/app/components/TextToSpeechButton.tsx
+++ part1/src/web/app/components/TextToSpeechButton.tsx
@@ -157,7 +157,7 @@ export default function TextToSpeechButton({ text, darkMode }: TextToSpeechButto
               <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M6.25 5C5.56 5 5 5.56 5 6.25v3.5a1.25 1.25 0 1 0 2.5 0v-3.5C7.5 5.56 6.94 5 6.25 5m3.5 0c-.69 0-1.25.56-1.25 1.25v3.5a1.25 1.25 0 1 0 2.5 0v-3.5C11 5.56 10.44 5 9.75 5" />
             </svg>
           ) : (
-            // Play icon
+            // Volume-up icon (when starting from beginning)
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="18"
@@ -165,7 +165,9 @@ export default function TextToSpeechButton({ text, darkMode }: TextToSpeechButto
               fill="currentColor"
               viewBox="0 0 16 16"
             >
-              <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814z" />
+              <path d="M11.536 14.01A8.47 8.47 0 0 0 14.026 8a8.47 8.47 0 0 0-2.49-6.01l-.708.707A7.48 7.48 0 0 1 13.025 8c0 2.071-.84 3.946-2.197 5.303z"/>
+              <path d="M10.121 12.596A6.48 6.48 0 0 0 12.025 8a6.48 6.48 0 0 0-1.904-4.596l-.707.707A5.48 5.48 0 0 1 11.025 8a5.48 5.48 0 0 1-1.61 3.89z"/>
+              <path d="M8.707 11.182A4.5 4.5 0 0 0 10.025 8a4.5 4.5 0 0 0-1.318-3.182L8 5.525A3.5 3.5 0 0 1 9.025 8 3.5 3.5 0 0 1 8 10.475zM6.717 3.55A.5.5 0 0 1 7 4v8a.5.5 0 0 1-.812.39L3.825 10.5H1.5A.5.5 0 0 1 1 10V6a.5.5 0 0 1 .5-.5h2.325l2.363-1.89a.5.5 0 0 1 .529-.06"/>
             </svg>
           )}
         </button>
